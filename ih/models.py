a93a25703d57a33e04c9688ecbeeeed2a3f4c1f7
chriscauley <chris@lablackey.com>
2018-04-26 03:29:28 +0000

    moving models from main into new ih app

--- ih/models.py
+++ ih/models.py
@@ -3,15 +3,23 @@ from django.db import models
 
 from lablackey.db.models import UserModel
 
+#! todo: move both these choices into the front end app
+
 ALIGNMENT_CHOICES = [
   ("good","good"),
   ("neutral","neutral"),
   ("evil","evil"),
 ]
 
+FREQUENCY_CHOICES = [ 1, 2, 3, 4, 5, 6, 7,"weekly","monthly","twice_monthly" ];
+FREQUENCY_CHOICES = [(str(a),str(a)) for a in FREQUENCY_CHOICES]
+
 class Task(UserModel):
-  hours = models.FloatField(default=24)
+  json_fields = ['id', #! TODO should be in self.as_json and self.as_json_list
+                 'name','per_time','frequency','alignment']
   name = models.CharField(max_length=128)
+  per_time = models.IntegerField(default=1)
+  frequency = models.CharField(max_length=32,choices=FREQUENCY_CHOICES)
   alignment = models.CharField(max_length=32,choices=ALIGNMENT_CHOICES)
   __unicode__ = lambda self: self.name
 
