d6d300b713ef05d03e28eb9b8b33fd032ee3eb2c
chriscauley <chris@lablackey.com>
2018-04-29 16:08:48 +0000

    TaskCompletionForm

--- ih/forms.py
+++ ih/forms.py
@@ -1,9 +1,14 @@
 from lablackey.forms import RequestUserModelForm
 
-from models import Task
+from models import Task, TaskCompletion
 
 class TaskForm(RequestUserModelForm):
   # field_overrides = { 'per_time': 'per_time' }
   class Meta:
     model = Task
     fields = ['name','per_time','frequency','alignment']
+
+class TaskCompletionForm(RequestUserModelForm):
+  class Meta:
+    model = TaskCompletion
+    fields = ['task','completed']
