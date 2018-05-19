b8a95636dc5a1a1010a45fa316b1bb0cbe814683
chriscauley <chris@lablackey.com>
2018-05-19 23:12:54 +0000

    links to goals at bottom of task form

--- main/static/ih/router.js
+++ main/static/ih/router.js
@@ -11,11 +11,28 @@ uR.router.add({
   "#/edit/([^/]+)/(\\d+)/": function(pathname,data) {
     var model_name = data.matches[1];
     var pk = data.matches[2];
+    var post_render;
+    if (model_name == "Task") {
+      function post_render() {
+        uR.pagination == undefined;
+        var goals = uR.db.ih.Goal.objects.filter({task:pk});
+        var results = goals.map(function(goal) {
+          return {
+            url: "#/edit/Goal/"+goal.pk+"/",
+            fields: [goal.targeted,goal.completed],
+          }
+        });
+        uR.pagination = {
+          results: results,
+        };
+      }
+    }
     uR.mountElement("ur-form",{
       schema: `/api/schema/ih.${model_name}Form/${pk}/`,
       method: "POST",
       ajax_success: () => uR.route("/"),
       cancel_function: () => uR.route("/"),
+      post_render: post_render,
     });
   },
   "^/$": uR.router.default_route,
