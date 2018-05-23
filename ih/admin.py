1ce706d9a087f50672cce8f90057ba9b2a55c781
chriscauley <chris@lablackey.com>
2018-05-23 23:21:19 +0000

    list_per_page

--- ih/admin.py
+++ ih/admin.py
@@ -8,8 +8,10 @@ from models import Task, Goal
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
   list_display = ["name","interval","per_time"]
+  list_per_page = 100
 
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
   list_filter = ["task"]
   list_display = ["__unicode__","targeted","completed"]
+  list_per_page = 100
