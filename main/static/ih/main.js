590582509da98695737b774be4986bd15c1f4e37
chriscauley <chris@lablackey.com>
2018-05-24 01:34:42 +0000

    target_time_interval to update any elements with data-target_time. This will be moved into lunchtime eventually

--- main/static/ih/main.js
+++ main/static/ih/main.js
@@ -39,4 +39,22 @@ uR.ready(function() {
   uR.config.name_overrides.completed = { type: "datetime-local" };
   uR.config.name_overrides.started = { type: "datetime-local" };
   uR.config.name_overrides.targeted = { type: "datetime-local" };
+
+  var tt_cache = {};
+  var target_time_interval = setInterval(function() {
+    var elements = document.querySelectorAll("[data-target_time]");
+    var now = new Date().valueOf();
+    // var count = 0 // #! TODO Revisit how frequently this is called... could become a performance issue
+    uR.forEach(elements,function(e,i) {
+      var target = e.dataset.target_time;
+      if (tt_cache[target] && tt_cache[target] > now) { return }
+      //count ++; // #! ibid
+      delta_ms = now-target;
+      var display = e.dataset.target_time_display = String.lunch.ms2hdelta(delta_ms)
+      tt_cache[target] = now;
+      if (delta_ms < 3600000) { return } // less than an hour needs immediate update
+      tt_cache[target] += 60000; // update in a minute
+    });
+    //console.log(count) #! ibid
+  },1000)
 });
