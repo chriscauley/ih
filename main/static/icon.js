7e5129d3a1090428ba326526baceea83c192c24a
chriscauley <chris@lablackey.com>
2018-06-02 23:24:58 +0000

    icons now work with uR.icon and have stylesheets such that .fi.fi-WHATEVER is an easy way to display flaticon.com icons

--- main/static/icon.js
+++ main/static/icon.js
@@ -1,28 +1,50 @@
-uR.config.name_overrides.icon = function() {
-  return {
-    type: 'select',
-    tagname: 'select-input',
-    choices: [
-      'beer',
-      'beers',
-      'cat-food',
-      'cat-feeder',
-      'cat-brush',
-      'cigarettes',
-      'dental-floss',
-      'drop',
-      'dryer',
-      'hop',
-      'lifeline-in-a-heart-outline',
-      'razor',
-      'selkirk-rex-cat',
-      'shirt',
-      'shower',
-      'skull',
-      'tooth-brush',
-      'smoking',
-      'washing-machine',
-      'washing-plate',
+(function() {
+  var ICON_CHOICES = [
+    'beer',
+    'beers',
+    'cat-food',
+    'cat-feeder',
+    'cat-brush',
+    'cigarettes',
+    'dental-floss',
+    'drop',
+    'dryer',
+    'hop',
+    'lifeline-in-a-heart-outline',
+    'razor',
+    'selkirk-rex-cat',
+    'shirt',
+    'shower',
+    'skull',
+    'tooth-brush',
+    'smoking',
+    'washing-machine',
+    'washing-plate',
+  ];
+
+  var styles = [
+    `.fi { background: center no-repeat; background-size: 100% auto; }`,
     ]
+  for (var choice of ICON_CHOICES) {
+    uR._icons[choice] = "fi fi_"+choice;
+    styles.push(`.fi_${choice} { background-image: url(/static/icons/${choice}.png);width: 1em; height: 1em; }`);
   }
-}
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet){
+    style.styleSheet.cssText = styles.join("\n");
+  } else {
+    style.appendChild(document.createTextNode(styles.join("\n")));
+  }
+  head.appendChild(style);
+
+  uR.config.name_overrides.icon = function() {
+    return {
+      type: 'select',
+      tagname: 'select-input',
+      choices: ICON_CHOICES,
+    }
+  }
+})()
