1e92edd4766cf16b467d8f691318625a680d99bb
chriscauley <chris@lablackey.com>
2018-06-13 15:28:56 +0000

    tweaking routes to match urls

--- main/static/router.js
+++ main/static/router.js
@@ -1,7 +1,7 @@
 uR.router.default_route = uR.auth.loginRequired(uR.router.routeElement("task-list"));
 uR.router.add({
   "^/$": uR.router.default_route,
-  "^/group/(\\d+)/$": uR.router.default_route,
-  "^/modechange/$": uR.router.routeElement("mode-viewer"),
+  "^/group/([^/]+)/$": uR.router.default_route,
+  "^/mode/$": uR.router.routeElement("mode-viewer"),
 });
 ih.ready(function() { uR.router.start() });
