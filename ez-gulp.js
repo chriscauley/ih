5d7fc0332929183f5eeb9a8825b35974b2e238cf
chriscauley <chris@lablackey.com>
2018-06-23 12:44:56 +0000

    using gulp rev to reversion stuff

--- ez-gulp.js
+++ ez-gulp.js
@@ -13,8 +13,10 @@ var rev = require('gulp-rev');
 
 module.exports = function(opts) {
   var build_tasks = [ 'cp-static'];
+  var results = []
   for (var key in opts.js) {
     (function(key) {
+      results.push(key + '-built.js');
       build_tasks.push("build-"+key);
       gulp.task('build-'+key, function () {
         if (key == "vendor") { // vendor files are already minified and babel-ifiied
@@ -23,12 +25,12 @@ module.exports = function(opts) {
             .pipe(gulp.dest(opts.DEST));
         }
         return gulp.src(opts.js[key])
-            .pipe(sourcemaps.init())
-            .pipe(riot())
-            .pipe(babel({ presets: ['es2017'] }))
-            .pipe(concat(key + '-built.js'))
-            .pipe(sourcemaps.write("."))
-            .pipe(gulp.dest(opts.DEST))
+          .pipe(sourcemaps.init())
+          .pipe(riot())
+          .pipe(babel({ presets: ['es2017'] }))
+          .pipe(concat(key + '-built.js'))
+          .pipe(sourcemaps.write("."))
+          .pipe(gulp.dest(opts.DEST))
       });
     })(key)
   }
@@ -36,6 +38,7 @@ module.exports = function(opts) {
   for (var key in opts.less) {
     (function(key) {
       build_tasks.push("build-"+key+"-css");
+      results.push(key+'-built.css')
       gulp.task("build-"+key+"-css", function () {
         return gulp.src(opts.less[key])
           .pipe(less({}))
@@ -59,6 +62,17 @@ module.exports = function(opts) {
       ncp(path.join(__dirname,r[0]),path.join(DEST,r[1]));
     });
   })
+
+  gulp.task('build-revision', function() {
+    gulp.src(results.map(s => path.join(opts.DEST,s)))
+      .pipe(rev())
+      .pipe(gulp.dest(opts.DEST))
+      .pipe(rev.manifest())
+      .pipe(gulp.dest(opts.DEST))
+  })
+
+  build_tasks.push('build-revision');
+  console.log(build_tasks);
 
   gulp.task('watch', build_tasks, function () {
     for (var key in opts.js) {
--- main/settings/10-apps.py
+++ main/settings/10-apps.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = (
   'django.contrib.staticfiles',
   'django.contrib.sites',
 
+  'gulp_rev',
   'compressor',
   'sorl.thumbnail',
 
